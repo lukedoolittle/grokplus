a66695dcde09a1bdba3aa6cbdb5bee3269540d31
LDoolittle <LDoolittle@musicnotes.com>
2015-08-18 12:14:19 -0500

    corrected error with put method

--- GrokPlus/grokAdapters/nupicAdapter.py
+++ GrokPlus/grokAdapters/nupicAdapter.py
@@ -11,7 +11,7 @@ class nupicAdapter(object):
     def permutations_runner(self, swarm_config, id):
       model_params = nupic.swarming.permutations_runner.runWithConfig(swarm_config, {'maxWorkers': 8, 'overwrite':True})
       model_params['timestamp'] = datetime.datetime.now()
-      self._repository.putJson(model_params, id)
+      self._repository.putJson(id, model_params)
       print("Finished creating model")
       # TODO do something with this result, with maybe a callback or something
       # model = ModelFactory.create(model_params)
